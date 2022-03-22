76db6f9b87f9ad77526b62faa442a4b7b8509b04
amirshokh <amirshoxmuxitdinov@gmail.com>
2022-03-22 10:30:13 +0500

    bugfix

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from typing import List
 
-from fastapi import FastAPI, Depends, status, HTTPException
+from fastapi import FastAPI, status
 
-from database import engine, metadata, database
-
-from articles import models
-from articles.schemas import ArticleScheme, GetArticleScheme
+from database import engine, metadata, database, Article
+from schemas import ArticleScheme, GetArticleScheme
 
 metadata.create_all(bind=engine)
 
@@ -20,3 +18,16 @@ async def startup():
 @app.on_event("shutdown")
 async def shutdown():
     await database.disconnect()
+
+
+@app.post("/articles/", status_code=status.HTTP_201_CREATED)
+async def insert_article(article: ArticleScheme):
+    query = Article.insert().values(title=article.title, description=article.description)
+    last_record_id = await database.execute(query)
+    return {**article.dict(), "id": last_record_id}
+
+
+@app.get("/articles/", response_model=List[GetArticleScheme])
+async def get_articles():
+    query = Article.select()
+    return await database.fetch_all(query=query)
