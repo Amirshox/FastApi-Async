df4ab3a4dd27c04cf108c0d254cc9fce76c971b1
amirshokh <amirshoxmuxitdinov@gmail.com>
2022-03-23 23:13:42 +0500

    crud async

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from fastapi import FastAPI, status
+from fastapi import FastAPI, status, HTTPException
 
 from database import engine, metadata, database, Article
 from schemas import ArticleScheme, GetArticleScheme
@@ -31,3 +31,28 @@ async def insert_article(article: ArticleScheme):
 async def get_articles():
     query = Article.select()
     return await database.fetch_all(query=query)
+
+
+@app.get("/articles/{id}/", response_model=GetArticleScheme)
+async def get_article(id: int):
+    query = Article.select().where(Article.c.id == id)
+    article = await database.fetch_one(query=query)
+
+    if not article:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Article not found")
+
+    return article
+
+
+@app.put("/articles/{id}/", response_model=ArticleScheme)
+async def update_article(id: int, article: ArticleScheme):
+    query = Article.update().where(Article.c.id == id).values(title=article.title, description=article.description)
+    await database.execute(query=query)
+    return {**article.dict(), "id": id}
+
+
+@app.delete("/articles/{id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_article(id: int):
+    query = Article.delete().where(Article.c.id == id)
+    await database.execute(query=query)
+    return {"message": "Article deleted"}
